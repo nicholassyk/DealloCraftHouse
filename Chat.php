4fe13eee1df9980c19a146c9e529177f8620c746
timchong97 <100067144@students.swinburne.edu.my>
2017-11-01 12:20:51 +0800

    Add files via upload

--- Chat.php
+++ Chat.php
@@ -1,3 +1,27 @@
+<?php
+$connect = mysqli_connect('localhost', 'root', '', 'message');
+if(mysqli_connect_errno())
+	{
+		die("Database connection failed: " .
+			mysqli_connect_error() .
+			" (" . mysqli_connect_errno(). ")"
+			);
+	}
+$_SESSION['posted'] = false;
+if(isset($_POST['submit'])){
+	$name = $_POST['fname'];
+	$msg = $_POST['message'];
+	$result = "";
+	if(!empty($msg) && !empty($name)){
+		$query = "INSERT INTO message (";
+		$query .= " name, messages";
+		$query .= ") VALUES (";
+		$query .= " '{$name}','{$msg}'";
+		$query .= ")";
+		$result = mysqli_query($connect, $query);
+	}
+}
+?>
 <!DOCTYPE html>
 <html>
     
@@ -6,6 +30,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>E-Market Portal System</title>
         <link rel="stylesheet" href="main.css">
+		<link rel="stylesheet" href="chat.css">
         <meta name="Chat" content="Chat">
     </head>
     <body>
@@ -26,7 +51,21 @@
 		<hr>
 		
         <div id="content">
-            <p>put content here</p>
+			<div id="thread">
+				<?php
+					$select = "SELECT * FROM message";
+					$q = mysqli_query($connect, $select);
+					while($row = mysqli_fetch_array($q, MYSQLI_ASSOC)){
+						echo $row['name']. ": ". $row['messages']."<br>";
+					}
+				?>
+			</div>
+            <form method="POST" action="Chat.php">
+				<label for= "fname">Name:</label>
+				<input type="text" name = "fname" id="fname"><br>
+					<textarea name="message" id="message" rows = "4" cols="35"></textarea><br>
+					<button type="submit" name="submit">Submit</button>
+			</form>
         </div>
    
      <footer>
